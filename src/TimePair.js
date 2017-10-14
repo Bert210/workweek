326f2959e7428437e0b8161f1f85a448c5d1747a
Robert <bert210@gmail.com>
2017-10-14 18:28:02 -0400

    Cleaned up TimeSetter

--- src/TimePair.js
+++ src/TimePair.js
@@ -1,11 +1,22 @@
 import React from 'react'
 import TimeSetter from './TimeSetter'
 
-const TimePair = ({onTimeChange}) =>(
-  <div>
-    <TimeSetter onTimeChange={(e) => onTimeChange({type:e.type, value:e.value, clock:"In"})}/>
-    <TimeSetter onTimeChange={(e) => onTimeChange({type:e.type, value:e.value, clock:"Out"})}/>
-  </div>
-)
+
+
+const TimePair = ({onTimeChange}) => {
+  let output = 0
+
+  let updateTime = (data) => {
+    onTimeChange(data)
+  }
+
+  return (
+    <div>
+      <TimeSetter id="in" onTimeChange={(e) => updateTime({type:e.type, value:e.value, clock:"In"})}/>
+      <TimeSetter id="out" onTimeChange={(e) => updateTime({type:e.type, value:e.value, clock:"Out"})}/>
+      <div className="timepair-output">{output}</div>
+    </div>
+  )
+}
 
 export default TimePair
--- src/TimeSetter.js
+++ src/TimeSetter.js
@@ -2,11 +2,44 @@ import React from 'react'
 
 
 const TimeSetter = ({match, onTimeChange}) => {
+  let hour = 8
+  let minute = 30
+  let isPM = false
 
+
+  const updateHour = (e) => {
+    hour = e.target.value
+    onTimeChange({type:"hour", value:hour})
+  }
+
+  const updateMinute = (e) => {
+    minute = e.target.value
+    onTimeChange({type:"minute", value:minute})
+  }
+
+  const updatePM = (e) => {
+    isPM = e.target.value
+    onTimeChange({type:"pm", value:isPM})
+  }
 
   return (
     <div>
-      <input id="hour" type="number" defaultValue="8" onChange={(e) => onTimeChange({type:"hour", value:e.target.value})}/>:<input id="minute" type="number" defaultValue="00"/> <input id="pm" type="checkbox" defaultChecked={false}/> PM
+      <input 
+        id="hour" 
+        type="number" 
+        defaultValue={hour} 
+        onChange={updateHour}/>
+      :
+      <input 
+        id="minute" 
+        type="number" 
+        defaultValue={minute}/> 
+      <input 
+        id="pm" 
+        type="checkbox" 
+        defaultChecked={isPM}/> PM
+
+        <div>{hour}:{minute} {isPM ? "PM" : "AM"}</div>
     </div>
   );
 }
