3b98ef71f426913b669bd68be84eb9a937f9dd7a
Robert <bert210@gmail.com>
2017-11-21 07:50:42 -0500

    Added workable form to all inputs

--- src/components/TimeView.js
+++ src/components/TimeView.js
@@ -4,62 +4,87 @@ import {connect} from 'react-redux'
 
 import {updateTime} from '../actions/time.js'
 
-
-  /*
-const TimeView = ({id, dayRef, inTime, outTime, totalTime, removeTime}) => {  
-  const handleChange = () => {{
-  return(
-  <div>
-    <form>
-      In:<input type="number" ref="inTimeHour" defaultValue={inTime.hour} />
-      <input type="number" defaultValue={inTime.minute} onChange={() => {}}/><input type="checkbox" />
-      <br/>
-      Out: <input type="number" value={outTime.hour} onChange={() => {}}/>
-      <input type="number" value={outTime.minute} onChange={() => {}}/>
-      <br/>
-      <span>Total: {totalTime.hour}:{totalTime.minute}</span>
-    </form>
-    
-    <button onClick={
-      () => { removeTime(id) } 
-      }>&times;</button> 
-  </div>
-)}
-*/
-
 class TimeView extends React.Component {
   constructor(props){
     super(props)
      
     let inTimeAMPM = this.props.inTime.hour >= 12 ? true : false;
-    let outTimeAMPM = this.props.outTime.hour >= 12 ? true: false; 
+    let outTimeAMPM = this.props.outTime.hour >= 12 ? true : false; 
 
 
     this.state = {
       inTime:{
         hour: this.props.inTime.hour,
         minute: this.props.inTime.minute,
-        am: inTimeAMPM
+        pm: inTimeAMPM
       },
       outTime: {
         hour: this.props.outTime.hour,
         minute: this.props.outTime.minute,
-        am: outTimeAMPM
+        pm: outTimeAMPM
+      },
+      totalTime: {
+        hour: this.props.totalTime.hour,
+        minute: this.props.totalTime.minute
       }
     }
+
   }
 
   handleChange = (value, id) => {
+    let {inTime, outTime} = this.state
     switch(id){
       case("inTimeHour"):
-        let inTime = {
+        let inTimeAMPM = value.target.valueAsNumber >= 12 ? true : false; 
+        inTime = {
           ...this.state.inTime,
-          hour: value.target.valueAsNumber
+          hour: value.target.valueAsNumber,
+          pm: inTimeAMPM
         }
-        this.setState({inTime})
         break;
+      case("inTimeMinute"):
+         inTime = {
+          ...this.state.inTime,
+          minute: value.target.valueAsNumber
+        }
+        break;
+      case("inTimeAMPM"):
+        inTime = {
+          ...this.state.inTime,
+          pm: value.target.value
+        }
+        console.log(value.target.checked)
+        break;
+      case("outTimeHour"):
+        let outTimeAMPM = value.target.valueAsNumber >= 12 ? true : false; 
+        outTime = {
+          ...outTime,
+          hour: value.target.valueAsNumber,
+          pm: outTimeAMPM
+        }
+
+        break;
+      case("outTimeMinute"):
+        outTime = {
+          ...outTime,
+          minute: value.target.valueAsNumber
+        }
+        break;
+      case("outTimeAMPM"):
+        outTime = {
+          ...outTime,
+          pm: value.target.checked
+        }
+        break;
+      default:
+        return;
     }
-    console.log(this.props.ide) 
+    let totalTime = {
+      hour: outTime.hour - inTime.hour,
+      minute: outTime.minute - inTime.minute
+    }
+    
+    this.setState({inTime, outTime, totalTime})
     //this.props.updateTime(this.props.id)
   }
 
@@ -69,13 +94,18 @@ class TimeView extends React.Component {
         <form>
           In:<input type="number" ref="inTimeHour" value={this.state.inTime.hour} onChange={(e) => {this.handleChange(e, "inTimeHour")}}/>
           <input type="number" ref="inTimeMinute" defaultValue={this.props.inTime.minute} onChange={() => {}}/>
-          <input type="checkbox" ref="inTimeAMPM" />
+          <label>PM:
+            <input type="checkbox" checked={this.state.inTime.pm} onChange={(e) => {this.handleChange(e, "inTimeAMPM")}} />
+          </label>
           <br/>
-          Out: <input type="number" value={this.props.outTime.hour} ref="outTimeHour" onChange={() => {}}/>
-          <input type="number" value={this.props.outTime.minute} ref="outTimeMinute" onChange={() => {}}/>
-          <input type="checkbox" ref="outTimeAMPM" />
+          Out: <input type="number" value={this.state.outTime.hour} onChange={(e) => {this.handleChange(e,"outTimeHour")}}/>
+          <input type="number" value={this.state.outTime.minute} onChange={(e) => {this.handleChange(e,"outTimeMinute")}}/>
+          
+          <label>PM: 
+            <input type="checkbox" checked={this.state.outTime.pm} onChange={ (e) => {this.handleChange(e, "outTimeAMPM")}}/>
+          </label>
           <br/>
-          <span>Total: {this.props.totalTime.hour}:{this.props.totalTime.minute}</span>
+          <span>Total: {this.state.totalTime.hour}:{this.state.totalTime.minute}</span>
         </form>
 
         <button onClick={
